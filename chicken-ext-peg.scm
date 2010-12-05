48b38a61175588735d4c19b51e3d1884a42c427b
alanpost <alanpost@fca3e652-9b03-0410-8d7b-ac86a6ce46c4>
2010-12-05 22:58:35 +0000

    genturfa'i: Add the genturfahi-peg library egg wrapper.
    This should have gone in with a previous commit, do it now instead!
    
    git-svn-id: https://code.call-cc.org/svn/chicken-eggs/release/4/genturfahi/trunk@21827 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

---
+++ chicken-ext-peg.scm
@@ -0,0 +1,95 @@
+;;;;
+;;;; genturfahi - lo la .ckim. ke pe'a jajgau ratcu ke'e genturfa'i
+;;;;            `-> A Scheme packrat parser.
+;;;;
+;;;; Copyright (c) 2010 ".alyn.post." <alyn.post@lodockikumazvati.org>
+;;;;
+;;;; Permission to use, copy, modify, and/or distribute this software for any
+;;;; purpose with or without fee is hereby granted, provided that the above
+;;;; copyright notice and this permission notice appear in all copies.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+;;;; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+;;;; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+;;;; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+;;;; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+;;;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+;;;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+;;;;
+
+(module genturfahi-peg
+  (genturfahi-env
+
+   genturfahi-peg
+   genturfahi-peg-gerna
+
+   ; samselpla
+   samselpla-hash-table
+   samselpla-hash-table*
+
+   samselpla-cmene->symbol
+   samselpla-cmene->symbol*
+
+   samselpla-smuni
+   samselpla-naselci
+   samselpla-je
+   samselpla-pajavni-cmene
+   samselpla-jonai-je
+   samselpla-jonai
+   samselpla-?
+   samselpla-*
+   samselpla-+
+   samselpla-&
+   samselpla-fanmo
+   samselpla-!
+   samselpla-cmene-sumti
+   samselpla-selci-naselci
+   samselpla-lerfu-selci
+   samselpla-lerfu-space
+   samselpla-lerfu-return
+   samselpla-lerfu-linefeed
+   samselpla-lerfu-newline
+   samselpla-lerfu-return
+   samselpla-lerfu-tab
+   samselpla-valsi-selci
+   samselpla-valsi-newline
+   samselpla-valsi-linefeed
+   samselpla-valsi-tab
+   samselpla-valsi-backslash
+   samselpla-valsi-single-quote
+   samselpla-valsi-double-quote
+   samselpla-valsi-lerfu
+   samselpla-klesi-selci
+   samselpla-denpabu
+   samselpla-samselpla
+   samselpla-samselpla-lerfu
+   samselpla-girzu-javni
+   samselpla-canlu)
+
+(import chicken)
+(import scheme)
+
+(include "chicken-declare.scm")
+
+;(require-extension utf8)
+
+(require-extension srfi-1)
+(require-extension srfi-13)
+(require-extension srfi-14)
+(require-extension srfi-69)
+(require-extension ports)
+(require-library sandbox)
+(require-library genturfahi)
+
+(import srfi-1)          ; unzip2
+(import srfi-13)         ; string-append, string-copy
+(import srfi-14)         ; character set library
+(import srfi-69)         ; basic hash tables
+(import ports)           ; Chicken Scheme srfi-6 and port extensions
+(import sandbox)
+(import genturfahi)
+
+(include "eval.scm")
+(include "samselpla.scm")
+(include "bootstrap.scm")
+(include "peg.scm"))
