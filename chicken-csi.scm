36225f7c5c8ab13fcf1d54a7b230afd6d92035ff
alanpost <alanpost@fca3e652-9b03-0410-8d7b-ac86a6ce46c4>
2010-12-05 22:49:12 +0000

    genturfa'i: Add chicken-csi.scm, for running this egg in the interpreter.
    git-svn-id: https://code.call-cc.org/svn/chicken-eggs/release/4/genturfahi/trunk@21826 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

---
+++ chicken-csi.scm
@@ -0,0 +1,40 @@
+(include "chicken-declare.scm")
+
+(use srfi-1)
+(use srfi-9)
+(use srfi-13)    ; String Libraries
+(use srfi-14)    ; Character Classes
+(use srfi-28)    ; Basic Format Strings
+(use srfi-37)    ; args-fold
+(use srfi-39)    ; parameter objects
+(use srfi-69)    ; hash tables
+(use extras)     ; format, pretty-print
+(use sandbox)    ; safe-eval
+
+(include "chicken-platform.scm")
+
+; genturfahi (lib)
+;
+(include "lerfu-porsi.scm")
+(include "javni-valsi.scm")
+(include "secuxna.scm")
+(include "nunjavni.scm")
+(include "morji.scm")
+(include "nunvalsi.scm")
+(include "genturfahi.scm")
+(include "version.scm")
+
+; genturfahi-peg
+;
+(include "eval.scm")
+(include "samselpla.scm")
+(include "bootstrap.scm")
+(include "peg.scm")
+
+; genturfahi (bin)
+;
+(include "license.scm")
+(include "main-srfi-37.scm")
+
+(main)
+(exit (secuxna-exit-status))
