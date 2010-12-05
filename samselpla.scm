dd40fd5d46c92f9edf3f5cba556f2b8dc37de768
alanpost <alanpost@fca3e652-9b03-0410-8d7b-ac86a6ce46c4>
2010-12-05 22:48:07 +0000

    genturfa'i: add '-' to gensym prefix.
    I think this is a bit easier to read, and I do have to read the
    compiler output from time to time.
    
    git-svn-id: https://code.call-cc.org/svn/chicken-eggs/release/4/genturfahi/trunk@21823 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- samselpla.scm
+++ samselpla.scm
@@ -32,7 +32,7 @@
   (hash-table-ref samselpla-hash-table
                   cmene
                   (lambda ()
-                    (let ((symbol (gensym (string->symbol cmene))))
+                    (let ((symbol (gensym (string->symbol (string-append cmene "-")))))
                       (hash-table-set! samselpla-hash-table
                                        cmene
                                        symbol)
@@ -42,7 +42,7 @@
   (hash-table-ref samselpla-hash-table*
                   cmene
                   (lambda ()
-                    (let ((symbol (gensym (string->symbol cmene))))
+                    (let ((symbol (gensym (string->symbol (string-append cmene "-")))))
                       (hash-table-set! samselpla-hash-table*
                                        cmene
                                        symbol)
