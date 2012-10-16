a84ee423499a2b8964a75806b9c9a533b161ae70
Alan Post <alanpost@sunflowerriver.org>
2012-10-16 07:14:24 -0600

    bump version to 1.0.3.

--- version.scm
+++ version.scm
@@ -1,6 +1,6 @@
 (define genturfahi-version-major 1)
 (define genturfahi-version-minor 0)
-(define genturfahi-version-patch 2)
+(define genturfahi-version-patch 3)
 
 ;(define genturfahi-version (format "~a.~a.~a"
 ;                             genturfahi-version-major
