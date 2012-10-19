590dba8ab0a3126ba31093acf99cc009ca7d5791
Alan Post <alanpost@sunflowerriver.org>
2012-10-19 07:44:46 -0600

    release genturfa'i 1.0.4.

--- version.scm
+++ version.scm
@@ -2,8 +2,7 @@
 (define genturfahi-version-minor 0)
 (define genturfahi-version-patch 4)
 
-;(define genturfahi-version (format "~a.~a.~a"
-;                             genturfahi-version-major
-;                             genturfahi-version-minor
-;                             genturfahi-version-patch))
-(define genturfahi-version "master")
+(define genturfahi-version (format "~a.~a.~a"
+                            genturfahi-version-major
+                            genturfahi-version-minor
+                            genturfahi-version-patch))
